8c025b6671754ef109fe0b2df7fa5f35cbb10fdc
lodeme <61187384+lodeme@users.noreply.github.com>
2024-04-20 18:10:13 +0200

    removed hyperland - not working on vm

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -42,10 +42,6 @@
     LC_TIME = "de_DE.UTF-8";
   };
 
-  # Enable Hyprland
-  programs.hyprland.enable = true;
-
-/*
   # Enable the X11 windowing system.
   services.xserver.enable = true;
 
@@ -58,7 +54,7 @@
     layout = "us";
     variant = "";
   };
-*/
+ 
   # Enable CUPS to print documents.
   services.printing.enable = true;
 
