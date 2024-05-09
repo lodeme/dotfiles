a5eda57e45705eeabf91b199210c4002cd95285a
lodeme <61187384+lodeme@users.noreply.github.com>
2024-05-09 21:51:36 +0200

    modularized vscode.nix

--- home/home.nix
+++ home/home.nix
@@ -14,67 +14,6 @@
     nixpkgs-fmt
   ];
 
-      programs.vscode = {
-      enable = true;
-      package = pkgs.vscode.fhs;
-      userSettings = {
-        "[markdown]" = {
-          "editor.defaultFormatter" = "DavidAnson.vscode-markdownlint";
-        };
-        "files.autoSave" = "afterDelay";
-        "files.autoSaveDelay" = 500;
-        "workbench.colorTheme" = "Gruvbox Dark Medium"; # Celestial | Ayu Dark | Correia Gruvbox Theme
-        "workbench.tree.indent" = 20;
-        "editor.cursorSmoothCaretAnimation" = "on";
-        "editor.inlineSuggest.suppressSuggestions" = false;
-        "workbench.panel.defaultLocation" = "right";
-        "editor.cursorSurroundingLines" = 10;
-        "editor.rulers" = [ 80 88 ];
-        "editor.fontFamily" = "Fira Code";
-        "editor.fontWeight" = 500;
-        "editor.fontLigatures" = true;
-      };
-      extensions =
-        with pkgs.vscode-extensions;
-          [
-            # THEMES
-            dracula-theme.theme-dracula
-            github.github-vscode-theme
-            jdinhlife.gruvbox
-
-            # GIT
-            mhutchie.git-graph
-            github.vscode-pull-request-github
-            github.vscode-github-actions
-            github.codespaces
-
-            ## TODO
-            gruntfuggly.todo-tree
-
-            # VIM
-            vscodevim.vim
-
-            # LANGUAGES
-            ## NIX
-            bbenoist.nix
-            jnoortheen.nix-ide
-
-            ## Markdown
-            yzhang.markdown-all-in-one
-            davidanson.vscode-markdownlint
-
-            ## C/C++
-            ms-vscode.cpptools
-
-            # Debuggers
-            vadimcn.vscode-lldb # C/C++ Rust
-
-            ## CMAKE
-            ms-vscode.cmake-tools
-
-          ]
-        ;
-    };
 
 
   imports = [
@@ -84,6 +23,7 @@
     ./git.nix
     ./nvim/nvim.nix
     ./hypr/hypr.nix
+    ./vscode.nix
   ];
 }
 
