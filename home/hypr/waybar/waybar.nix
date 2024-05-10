97bde7a6a990a9d2de68760e6df8cd560f739989
lodeme <61187384+lodeme@users.noreply.github.com>
2024-05-10 09:42:57 +0200

    fix issue

--- home/hypr/waybar/waybar.nix
+++ home/hypr/waybar/waybar.nix
@@ -1,7 +1,7 @@
 { config, pkgs, lib, ... }:
 
 {
-  home.activation.cleanupHyprlandDir = lib.hm.dag.entryBefore ["writeBoundary"] ''
+  home.activation.cleanupWaybarDir = lib.hm.dag.entryBefore ["writeBoundary"] ''
     # Remove the existing directory if it exists
     if [ -d $HOME/.config/waybar ]; then
       rm -rf $HOME/.config/waybar
